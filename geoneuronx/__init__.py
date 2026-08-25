1f88beb9de2811baef43e4c18d87f242c450572b
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-08-25 09:17:42 +0300

    Expose GeoNeuronX core API

---
+++ geoneuronx/__init__.py
@@ -0,0 +1,27 @@
+from .core import (
+    AdaptiveAIS,
+    amuse,
+    condition_number,
+    effective_rank,
+    make_temporal_sources,
+    observe_through_morphology,
+    oja_first_component,
+    path_transfer_matrix,
+    pca_scores,
+    recovery_score,
+    soma_from_branches,
+)
+
+__all__ = [
+    "AdaptiveAIS",
+    "amuse",
+    "condition_number",
+    "effective_rank",
+    "make_temporal_sources",
+    "observe_through_morphology",
+    "oja_first_component",
+    "path_transfer_matrix",
+    "pca_scores",
+    "recovery_score",
+    "soma_from_branches",
+]
